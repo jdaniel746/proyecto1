9a24d45bf3898806c60162fa3c800f1c835a9a74
jose.alvarado <jose.alvarado@smartmatic.com>
2022-07-20 12:14:16 -0400

    le quite la s a titulo y puse hola

--- java.js
+++ java.js
@@ -3,7 +3,7 @@ let contador = 0;
 setInterval(() => {
 	contador++
 	if(contador <10){
-		document.getElementById('titulos').innerHTML = 'epale '  + contador;
+		document.getElementById('titulo').innerHTML = 'hola '  + contador;
 	} else{
 		//alert("te pasaste brother");
 	}
