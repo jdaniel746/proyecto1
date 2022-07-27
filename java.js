46d3c15516799bb5ce6abda4b9c86317ec1d65e7
josep <paul.mendez3115@gmail.com>
2022-07-27 13:08:26 -0400

    agregue un nuero

--- java.js
+++ java.js
@@ -3,7 +3,7 @@ let contador = 0;
 setInterval(() => {
 	contador++
 	if(contador <10){
-		document.getElementById('titulo').innerHTML = 'sinceramente no se que cambiar '  + contador;
+		document.getElementById('titulo').innerHTML = 'sinceramente no se que cambiar 5 '  + contador;
 	} else{
 	}
 }, 1000)
