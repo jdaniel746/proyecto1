ad8d630c45cf0fe7168dbcfef1bb2d6673863258
josep <paul.mendez3115@gmail.com>
2022-07-27 12:59:15 -0400

    cambie el hola

--- java.js
+++ java.js
@@ -3,7 +3,7 @@ let contador = 0;
 setInterval(() => {
 	contador++
 	if(contador <10){
-		document.getElementById('titulo').innerHTML = 'hola '  + contador;
+		document.getElementById('titulo').innerHTML = 'sinceramente no se que cambiar '  + contador;
 	} else{
 	}
 }, 1000)
